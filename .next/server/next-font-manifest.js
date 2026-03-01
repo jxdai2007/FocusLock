e4b3b9550bda3f70b91b511b97c3180aa6d6ad32
jxdai2007 <jxdaiship@gmail.com>
2026-03-01 15:29:34 -0800

    fixed git overtracking

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/jollenshoulddai/Desktop/FocusLock/app/layout":["static/media/9a4ee768fed045da-s.p.woff2","static/media/27834908180db20f-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"L/QKfZUEPPYFhRnf4FBPcZUv0nyUG9JMS4ijQ0XkKf0=\"}"
